ab489e838c6b9f523fb0e3e18c4035af9cdf6c92
RomainPlayez <165997408+RomainPlayez@users.noreply.github.com>
2024-05-07 09:19:41 +0200

    Update tp4.c

--- tp4.c
+++ tp4.c
@@ -12,3 +12,17 @@ T_Sommet *creerSommet(int element){
 T_Arbre insererElement(T_Arbre abr, int element){
     
 }
+
+bool validiteElements(int nombre_elements){
+    
+    bool inserer;
+    inserer = true;
+    if(nombre_elements==0){
+        inserer = false;
+    }else if(nombre_elements>0){
+        inserer = true;
+    }else{
+        inserer = false;
+    }
+
+}
