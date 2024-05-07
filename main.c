11a11c8aef1a8fc1bd3b5b29f07b8a032938cedd
RomainPlayez <165997408+RomainPlayez@users.noreply.github.com>
2024-05-07 09:18:49 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,43 +9,61 @@ int main(){
     printf("NF16 - P024 - TP4 : Les Arbres Binaires de Recherche\n");
     printf("Laborie Leo - Playez Romain\n");
     bool continuer=true;
+    int nombre_elements;
     int choix;
 
     while (continuer==true){
 
         printf("\n------------------------------------------------\n");
-        printf("1. Inserer N elements\n");
-        printf("2. Rechercher un element\n");
-        printf("3. Afficher tous les sommets\n");
-        printf("4. Afficher tous les elements\n");
-        printf("5. Supprimer un element\n");
-        printf("6. Afficher la taille de la memoire\n");
-        printf("7. Quitter\n");
+        printf("1. Creer un sommet");
+        printf("2. Inserer N elements\n");
+        printf("3. Rechercher un element\n");
+        printf("4. Afficher tous les sommets\n");
+        printf("5. Afficher tous les elements\n");
+        printf("6. Supprimer un element\n");
+        printf("7. Afficher la taille de la memoire\n");
+        printf("8. Quitter\n");
         printf("\n------------------------------------------------\n");
 
         scanf("%d", &choix);
 
         switch(choix){
-            case 1: //Inserer N elements
-                
-                break;
-            case 2: //Rechercher un element
-                
-                break;
-            case 3: //Afficher tous les sommets
-                        
-                break;
-            case 4: //Afficher tous les elements
+            case 1: //Creer un sommet
 
                 break;
-            case 5: //Supprimer un element
+
+            case 2: //Inserer N elements
+                printf("Veuillez saisir le nombre d'elements a inserer\n");
+                scanf("%d", &nombre_elements);
+                validiteElements(nombre_elements);
                 
+                if inserer==true{
+                    insererElement(nombre_elements);
+                }else{
+                    printf("Vous ne pouvez construire un arbre qu'avec un nombre d'elements positif");
+                }
+
                 break;
-            case 6: //Afficher la taille de la memoire
                 
+            case 3: //Rechercher un element
+
+                break;
+            case 4: //Afficher tous les sommets
+
                 break;
 
-            case 7: //Quitter
+            case 5: //Afficher tous les elements
+
+                break;
+            case 6: //Supprimer un element
+
+                break;
+
+            case 7: //Afficher la taille de la memoire
+
+                break;
+
+            case 8: //Quitter
                 continuer = false;
 
                 break;
