100a44d5131854a6cbb460d8af18ecb5de88bb5f
RomainPlayez <165997408+RomainPlayez@users.noreply.github.com>
2024-05-09 11:20:50 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include "tp4.c"
 #include "tp4.h"
+
 
 
 int main(){
@@ -13,32 +14,57 @@ int main(){
     T_Arbre arbre;
     arbre = NULL;
     int choix, element, nombre_elements, element_recherche, element_suppression;
-
+    char choix_suppression;
     while (continuer==true){
 
         printf("\n------------------------------------------------\n");
-        printf("1. Creer un sommet");
+        printf("1. Creer un sommet\n");
         printf("2. Inserer N elements\n");
         printf("3. Rechercher un element\n");
         printf("4. Afficher tous les sommets\n");
         printf("5. Afficher tous les elements\n");
         printf("6. Supprimer un element\n");
         printf("7. Afficher la taille de la memoire\n");
-        printf("8. Quitter\n");
+        printf("8. Quitter");
         printf("\n------------------------------------------------\n");
 
         scanf("%d", &choix);
 
         switch(choix){
+
             case 1: //Creer un sommet
-                printf("Veuillez saisir l'element du sommet a creer\n");
-                scanf("%d", &element);
-                creer = validiteSommet(element);
-                if (creer == true){
-                    creerSommet(element);
+                vide = arbreNonVide(arbre);
+                if(vide == true){
+                    printf("Veuillez saisir l'element du sommet a creer\n");
+                    scanf("%d", &element);
+                    creer = validiteSommet(element);
+                    if (creer == true){
+                        creerSommet(element);
+                    }else{
+                        printf("Vous ne pouvez construire un sommet qu'avec un element corresppondant a un nombre entier\n");
+                    }
                 }else{
-                    printf("Vous ne pouvez construire un sommet qu'avec un element corresppondant a un nombre entier");
+                    printf("Un arbre existe deja, voulez vous le supprimer pour en creer un nouveau ? (Y/N)\n");
+                    scanf("%c", &choix_suppression);
+                    while(choix_suppression != 'Y' || choix_suppression != 'N'){
+                        printf("Un arbre existe deja, voulez vous le supprimer pour en creer un nouveau ? (Y/N)\n");
+                        scanf("%c", &choix_suppression);
+                    }
+                    if(choix_suppression == 'Y'){
+                        //supprimer proprement l'arbre precedent
+                        printf("Veuillez saisir l'element du sommet a creer\n");
+                        scanf("%d", &element);
+                        creer = validiteSommet(element);
+                        if (creer == true){
+                            creerSommet(element);
+                        }else{
+                            printf("Vous ne pouvez construire un sommet qu'avec un element corresppondant a un nombre entier\n");
+                        }
+                    }else{
+                        printf("Vous avez choisi de garder l'arbre precedent, vous pouvez utiliser les autres fonctionnalites du menu\n");
+                    }
                 }
+
                 break;
 
             case 2: //Inserer N elements
@@ -65,6 +91,7 @@ int main(){
                     printf("Veuillez dans un premier temps creer un arbre");
                 }
                 break;
+
             case 4: //Afficher tous les sommets
                 vide = arbreNonVide(arbre);
                 if(vide == false){
@@ -82,6 +109,7 @@ int main(){
                     printf("Veuillez dans un premier temps creer un arbre");
                 }
                 break;
+
             case 6: //Supprimer un element
                 printf("Veuillez saisir l'element que vous souhaitez supprimer\n");
                 scanf("%d", &element_suppression);
@@ -95,13 +123,26 @@ int main(){
                 break;
 
             case 7: //Afficher la taille de la memoire
+                vide = arbreNonVide(arbre);
+                if(vide == false){
+                    tailleMemoire(arbre);
+                }else{
+                    printf("Veuillez dans un premier temps creer un arbre");
+                }
 
                 break;
 
             case 8: //Quitter
-                continuer = false;
+                vide = arbreNonVide(arbre);
+                if(vide == false){
+                    libererMemoire(arbre);
+                    continuer = false;
+                }else{
+                    continuer = false;
+                }
 
                 break;
+
             default :
                 printf("Ce choix n'est pas valide dans le menu precedent...");
                 break;
@@ -111,3 +152,4 @@ int main(){
 
     return 0;
 }
+
