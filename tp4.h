76bf8a14e29de2ad546b8cb9d934af7894ac22a8
RomainPlayez <165997408+RomainPlayez@users.noreply.github.com>
2024-06-02 16:09:01 +0200

    Update tp4.h

--- tp4.h
+++ tp4.h
@@ -14,38 +14,6 @@ struct Sommet* filsDroit;
 
 typedef T_Sommet* T_Arbre;
 
-T_Sommet *creerSommet(int element);
-
-T_Arbre insererElement(T_Arbre abr, int element);
-
-T_Sommet *rechercherElement(T_Arbre abr, int element);
-
-void afficherSommets(T_Arbre abr);
-
-void afficherElements(T_Arbre abr);
-
-T_Arbre supprimerElement(T_Arbre abr, int element, T_Sommet *pere);
-
-bool validiteSommet(int element);
-
-bool validiteElements(int nombre_elements);
-
-bool arbreNonVide(T_Arbre abr);
-
-void tailleMemoire(T_Arbre abr);
-
-size_t calculerTailleNoeud(T_Sommet* sommet);
-
-size_t calculerTailleArbre(T_Arbre abr);
-
-int compterNombreElements(T_Sommet* sommet);
-
-void libererMemoire(T_Arbre abr);
-
-void insertion_sommet(T_Arbre abr, T_Sommet* sommet);
-
-T_Arbre choixCreationArbre(T_Arbre abr);
-
 T_Arbre appelCreerSommet(T_Arbre arbre);
 
 T_Arbre appelInsererNElement(T_Arbre arbre);
@@ -61,5 +29,37 @@ T_Arbre appelSupprimerElement(T_Arbre arbre);
 void appelTailleMemoire(T_Arbre arbre);
 
 bool appelQuitter(T_Arbre arbre);
+
+T_Sommet *creerSommet(int element);
+
+T_Arbre insererElement(T_Arbre abr, int element);
+
+T_Sommet *rechercherElement(T_Arbre abr, int element);
+
+void afficherSommets(T_Arbre abr);
+
+void afficherElements(T_Arbre abr);
+
+T_Arbre supprimerElement(T_Arbre abr, int element, T_Sommet *pere);
+
+void tailleMemoire(T_Arbre abr);
+
+size_t calculerTailleNoeud(T_Sommet* sommet);
+
+size_t calculerTailleArbre(T_Arbre abr);
+
+int compterNombreElements(T_Sommet* sommet);
+
+void libererMemoire(T_Arbre abr);
+
+T_Arbre choixCreationArbre(T_Arbre abr);
+
+void insertion_sommet(T_Arbre abr, T_Sommet* sommet);
+
+bool validiteSommet(char element);
+
+bool validiteElements(char nombre_elements);
+
+bool arbreNonVide(T_Arbre abr);
 
 #endif
